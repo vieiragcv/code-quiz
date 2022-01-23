77cc4645004f6510bbe625101510377f6e75606e
vieiragcv <95590262+vieiragcv@users.noreply.github.com>
2022-01-23 14:02:20 -0500

    feature to cycle to next question after answering is working

--- assets/script.js
+++ assets/script.js
@@ -1,35 +1,51 @@
-//each object will house a question to be prompted on screen. 
+/* -------------------------------------------------------------
+-                   GLOBAL VARIABLES                           
+-------------------------------------------------------------*/
 
 var  q1 = 'Commonly used datatypes DO NOT include:';
 var q1opts = ['String','Booleans', 'Alerts', 'Numbers'];
 
-var q2 = 'Some text here asking a question to the user';
-var q2opts = ['1st Answer', '2nd Answer', '3rd Answer', '4th Answer'];
+var q2 = 'Question Number 2 Prompted';
+var q2opts = ['Q2 A1', 'Q2 A2', 'Q2 A3', 'Q2 A4'];
 
-var q3 = 'Some text here asking a question to the user';
-var q3opts = ['1st Answer', '2nd Answer', '3rd Answer', '4th Answer'];
+var q3 = 'Question Number 3 Prompted';
+var q3opts = ['Q3 A1', 'Q3 A2', 'Q3 A3', 'Q3 A4'];
 
-var q4 = 'Some text here asking a question to the user';
-var q4opts = ['1st Answer', '2nd Answer', '3rd Answer', '4th Answer'];
+var q4 = 'Question Number 4 Prompted';
+var q4opts = ['Q4 A1', 'Q4 A2', 'Q4 A3', 'Q4 A4'];
 
-var answers = [0, 3, 2, 2]
-// the followign arrays will house all questions and answers
+var correctAnswers = ['btn-0','btn-3','btn-2','btn-1']
+
+var correctCount = 0;
+var questionCount = 1;
 
 var questionArr = [q1, q2, q3, q4];
 var optionsArr = [q1opts, q2opts, q3opts, q4opts];
 
-var checkAnswer = function (event) {
+var mainContainer = document.querySelector("#main-container");
+var footerContainer = document.querySelector("#footer");
+
+/*----------------------------------------------------------------------
+-                       ANSWER VALIDATION
+----------------------------------------------------------------------*/
+
+var checkAnswer = function (event) {          // Working only for first question
   var targetEl = event.target;
-  console.log(targetEl.id);
-  if (targetEl.id === answers[0]) {           // need to wrap this in a FOR - each question
-  window.alert("HORRAAAAYY");
+  if (targetEl.id === correctAnswers[0]) {
+    var footerEl = createResultFooter('Correct!');  
+    footerContainer.appendChild(footerEl);
+    correctCount++;
   }
-  else {
-    window.alert("WRONG!");
-    console.log(targetEl.id);
-    console.log(answers[0]);
+  else {  
+    var footerEl = createResultFooter('Wrong');
+    footerContainer.appendChild(footerEl);
   }
+  footerEl.addEventListener('click',nextQuestion); // TEMPORARY - need to click on footer to load next question. should be a timed event (2 second delay)
 }
+
+/*----------------------------------------------------------------------
+-                             SUPPORT FUNCTIONS     
+---------------------------------------------------------------------*/
 
 var createButton = function (value) {
   var optEl = document.createElement("div");
@@ -42,34 +58,57 @@ var createButton = function (value) {
   return optEl;
 }
 
-var mainContainer = document.querySelector("#main-container");
-
-// creates a <div> <h3> QuestionArr['i'] </h3> </div>
-
-// for (i = 0; i < questionArr.length; i++) {                     // for to cycle through each question
-
-var qEl = document.createElement("div");  
-qEl.className = "row";
-qEl.innerHTML = '<h3>' + questionArr[0] + '</h3>';          // <h3> Question Text </h3>
-mainContainer.appendChild(qEl);                           // <div><h3></h3></div>
- 
-for (i=0; i < 4; i++){
-  var optEl = document.createElement("div");
-  optEl.className ="row";
-  var qButton = document.createElement("button");
-  qButton.className = "btn btn-primary btn-lg";
-  qButton.type = "button";
-  qButton.id = i;
-  qButton.textContent = q1opts[i];
-  optEl.appendChild(qButton);
-  mainContainer.appendChild(optEl);
-}
-
-
 var nextQuestion = function () {
   var clearQuestion = document.getElementById("main-container");
   clearQuestion.innerHTML = '';
+  var clearFooter = document.getElementById("footer");
+  clearFooter.innerHTML = '';
+  promptQuestion(questionCount);
+  questionCount++;
   return;
 };
 
-qEl.addEventListener('click', checkAnswer);
+var createResultFooter = function (value) {
+  var footerEl = document.createElement("div");
+  footerEl.className = "row";
+  footerEl.innerHTML = "<h3>" + value + "</h3>";
+  return footerEl;
+}
+// creates a <div> <h3> QuestionArr['i'] </h3> </div>
+
+/*------------------------------------------------------------------------
+-                         MAIN FUNCTION
+-------------------------------------------------------------------------*/
+
+var promptQuestion = function (value) {
+
+  var qEl = document.createElement("div");
+  qEl.className = "row";
+  qEl.innerHTML = '<h3>' + questionArr[value] + '</h3>';          // <h3> Question Text </h3>
+  mainContainer.appendChild(qEl);                           // <div><h3></h3></div>
+  
+  for (i=0; i < 4; i++){                                  // adds all 4 buttons to screen
+    var optEl = document.createElement("div");
+    optEl.className ="row";
+    var qButton = document.createElement("button");
+    qButton.className = "btn btn-primary btn-lg";
+    qButton.type = "button";
+    qButton.id = 'btn-'+i;
+    qButton.textContent = optionsArr[value][i];
+    optEl.appendChild(qButton);
+    mainContainer.appendChild(optEl);
+  }
+
+  var option1 = document.querySelector("#btn-0");
+  var option2 = document.querySelector("#btn-1");
+  var option3 = document.querySelector("#btn-2");
+  var option4 = document.querySelector("#btn-3");
+
+  option1.addEventListener('click', checkAnswer);
+  option2.addEventListener('click', checkAnswer);
+  option3.addEventListener('click', checkAnswer);
+  option4.addEventListener('click', checkAnswer);
+
+}
+
+promptQuestion(0);
