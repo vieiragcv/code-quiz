9060811b9f85a2f6dcb349afa5bb47aeb8f1c734
vieiragcv <95590262+vieiragcv@users.noreply.github.com>
2022-01-23 01:06:36 -0500

    generating first question and answer options

--- assets/script.js
+++ assets/script.js
@@ -1,127 +1,64 @@
 //each object will house a question to be prompted on screen. 
 
-var question1 = {
-  question: 'Commonly used datatypes DO NOT include:',
-  option1: 'String',
-  option2: 'Booleans',
-  option3: 'Alerts',
-  option4: 'Numbers',
-  answerId: 3
-};
+var  q1 = 'Commonly used datatypes DO NOT include:';
+var q1opts = ['String','Booleans', 'Alerts', 'Numbers'];
 
-var question2 = {
-  question: 'Commonly used datatypes DO NOT include:',
-  option1: 'String',
-  option2: 'Booleans',
-  option3: 'Alerts',
-  option4: 'Numbers',
-  answerId: 1
-};
-
-var question3 = {
-  question: 'Commonly used datatypes DO NOT include:',
-  option1: 'String',
-  option2: 'Booleans',
-  option3: 'Alerts',
-  option4: 'Numbers',
-  answerId: 4
-};
-
-var question4 = {
-  question: 'Commonly used datatypes DO NOT include:',
-  option1: 'String',
-  option2: 'Booleans',
-  option3: 'Alerts',
-  option4: 'Numbers',
-  answerId: 2
-};
-
-// this array will house all questions prompted at the screen. 
-
-var questionsArr = [question1, question2, question3, question4];
-console.log(questionsArr);
-
-// CREATE HTML FOR A QUESTIONS AND ITS ANSWERS
+var q2 = 'Some text here asking a question to the user';
+var q2opts = ['1st Answer', '2nd Answer', '3rd Answer', '4th Answer'];
 
 
+var q3 = 'Some text here asking a question to the user';
+var q3opts = ['1st Answer', '2nd Answer', '3rd Answer', '4th Answer'];
+
+
+var q4 = 'Some text here asking a question to the user';
+var q4opts = ['1st Answer', '2nd Answer', '3rd Answer', '4th Answer'];
+
+// the followign arrays will house all questions and answers
+
+var questionArr = [q1, q2, q3, q4];
+var optionsArr = [q1opts, q2opts, q3opts, q4opts];
+
+var createButton = function (value) {
+  var optEl = document.createElement("div");
+  optEl.className ="row";
+  var qButton = document.createElement("button");
+  qButton.className = "btn btn-primary btn-lg";
+  qButton.type = "button";
+  qButton.textContent = value;
+  optEl.appendChild(qButton);
+  console.log(optEl);
+  return optEl;
+}
 
 var mainContainer = document.querySelector("#main-container");
 
+// creates a <div> <h3> QuestionArr['i'] </h3> </div>
 
-  var questEl = document.createElement("div");
-  questEl.className = "row";
-  questEl.innerHTML = "<h3>" + questionsArr[0].question + "</h3>";
+// for (i = 0; i < questionArr.length; i++) {                     // for to cycle through each question
 
-  mainContainer.appendChild(questEl);
-
-    optEl1 = document.createElement("div");
-    optEl1.className = "row";
-
-    var questButton1 = document.createElement("button");
-    questButton1.className = "btn btn-primary btn-lg";
-    questButton1.type = "button";
-    questButton1.textContent = questionsArr[0].option1;
-
-    optEl1.appendChild(questButton1);
-    mainContainer.appendChild(optEl1);
-
-    optEl2 = document.createElement("div");
-    optEl2.className = "row";
-
-    var questButton2 = document.createElement("button");
-    questButton2.className = "btn btn-primary btn-lg";
-    questButton2.type = "button";
-    questButton2.textContent = questionsArr[0].option2;
-
-    optEl2.appendChild(questButton2);
-    mainContainer.append(optEl2);
-
-    optEl3 = document.createElement("div");
-    optEl3.className = "row";
-
-    var questButton3 = document.createElement("button");
-    questButton3.className = "btn btn-primary btn-lg";
-    questButton3.type = "button";
-    questButton3.textContent = questionsArr[0].option3;
-
-    optEl3.appendChild(questButton3);
-    mainContainer.appendChild(optEl3);
-
-    optEl4 = document.createElement("div");
-    optEl4.className = "row";
-
-    var questButton4 = document.createElement("button");
-    questButton4.className = "btn btn-primary btn-lg";
-    questButton4.type = "button";
-    questButton4.textContent = questionsArr[0].option4;
-
-    optEl4.appendChild(questButton4);
-    mainContainer.appendChild(optEl4);
-
-
-
-  
-
-  optEl1.appendChild(questButton1);
-
-  mainContainer.appendChild(opt1E);
-
-  console.log(mainContainer);
-
+var qEl = document.createElement("div");  
+qEl.className = "row";
+qEl.innerHTML = '<h3>' + questionArr[0] + '</h3>';          // <h3> Question Text </h3>
+console.log(qEl);
+mainContainer.appendChild(qEl);                           // <div><h3></h3></div>
+console.log(mainContainer);
  
+for (i=0; i < 4; i++){
+  var optEl = document.createElement("div");
+  optEl.className ="row";
+  var qButton = document.createElement("button");
+  qButton.className = "btn btn-primary btn-lg";
+  qButton.type = "button";
+  qButton.textContent = q1opts[i];
+  optEl.appendChild(qButton);
 
+  mainContainer.appendChild(optEl);
+}
 
 
 var nextQuestion = function () {
   var clearQuestion = document.getElementById("main-container");
   clearQuestion.innerHTML = '';
-}
-
-
-
-/*
-var createElements = function() {
-    for (i = 0; i < 4; i++) {
-    }
-  }
-*/
+  return;
+};
