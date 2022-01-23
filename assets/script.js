f9254ae4210fda8aa97bc730f4c2b8c71635843a
vieiragcv <95590262+vieiragcv@users.noreply.github.com>
2022-01-23 02:08:29 -0500

    can click on a answer, but not yet verify if its correct

--- assets/script.js
+++ assets/script.js
@@ -6,18 +6,30 @@ var q1opts = ['String','Booleans', 'Alerts', 'Numbers'];
 var q2 = 'Some text here asking a question to the user';
 var q2opts = ['1st Answer', '2nd Answer', '3rd Answer', '4th Answer'];
 
-
 var q3 = 'Some text here asking a question to the user';
 var q3opts = ['1st Answer', '2nd Answer', '3rd Answer', '4th Answer'];
-
 
 var q4 = 'Some text here asking a question to the user';
 var q4opts = ['1st Answer', '2nd Answer', '3rd Answer', '4th Answer'];
 
+var answers = [0, 3, 2, 2]
 // the followign arrays will house all questions and answers
 
 var questionArr = [q1, q2, q3, q4];
 var optionsArr = [q1opts, q2opts, q3opts, q4opts];
+
+var checkAnswer = function (event) {
+  var targetEl = event.target;
+  console.log(targetEl.id);
+  if (targetEl.id === answers[0]) {           // need to wrap this in a FOR - each question
+  window.alert("HORRAAAAYY");
+  }
+  else {
+    window.alert("WRONG!");
+    console.log(targetEl.id);
+    console.log(answers[0]);
+  }
+}
 
 var createButton = function (value) {
   var optEl = document.createElement("div");
@@ -27,7 +39,6 @@ var createButton = function (value) {
   qButton.type = "button";
   qButton.textContent = value;
   optEl.appendChild(qButton);
-  console.log(optEl);
   return optEl;
 }
 
@@ -40,9 +51,7 @@ var mainContainer = document.querySelector("#main-container");
 var qEl = document.createElement("div");  
 qEl.className = "row";
 qEl.innerHTML = '<h3>' + questionArr[0] + '</h3>';          // <h3> Question Text </h3>
-console.log(qEl);
 mainContainer.appendChild(qEl);                           // <div><h3></h3></div>
-console.log(mainContainer);
  
 for (i=0; i < 4; i++){
   var optEl = document.createElement("div");
@@ -50,9 +59,9 @@ for (i=0; i < 4; i++){
   var qButton = document.createElement("button");
   qButton.className = "btn btn-primary btn-lg";
   qButton.type = "button";
+  qButton.id = i;
   qButton.textContent = q1opts[i];
   optEl.appendChild(qButton);
-
   mainContainer.appendChild(optEl);
 }
 
@@ -62,3 +71,5 @@ var nextQuestion = function () {
   clearQuestion.innerHTML = '';
   return;
 };
+
+qEl.addEventListener('click', checkAnswer);
